30c9954173f8884bf4011de1041f642eb7816d11
Pierre PEROL <pperol@e3r3p14.clusters.42paris.fr>
2022-11-24 18:10:19 +0100

    update errors

--- pipex.c
+++ pipex.c
@@ -3,20 +3,50 @@
 /*                                                        :::      ::::::::   */
 /*   pipex.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pperol <pperol@student.42.fr>              +#+  +:+       +#+        */
+/*   By: pperol <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/09/29 16:22:10 by pperol            #+#    #+#             */
-/*   Updated: 2022/10/13 15:45:23 by pperol           ###   ########.fr       */
+/*   Created: 2022/11/14 15:46:12 by pperol            #+#    #+#             */
+/*   Updated: 2022/11/24 17:26:33 by pperol           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "pipex.h"
+/*
 
-int	ft_error(char *msg)
-{
-	write(2, msg, ft_strlen(msg));
-	return (1);
-}
+We can divide the pipex workload on different children, while the parent waits 
+for the job to be done and supervise the children’s status.
+We will thus have to fork twice, and assign child1 to execute cmd1, and child2 
+to execute cmd2. 
+The parent will wait at the end. 
+
+# ./pipex infile cmd1 cmd2 outfile
+
+pipe()
+ |
+ |-- fork()
+ :    |
+ :    |-- child1 // cmd1
+ :    :     |--dup2()
+ :    :     |--close end[0]
+ :    :     |--execve(cmd1)
+ :    :
+ |-- fork()
+      |
+      |-- child2 (cf. parent) // cmd2
+      :     |--dup2()
+      :     |--close end[1]
+      :     |--execve(cmd2)
+
+Cf.:
+	--- http://www.man-linux-magique.net/man7/pipe.html
+	--- http://www.man-linux-magique.net/man2/pipe.html
+	--- http://www.man-linux-magique.net/man2/fork.html
+	--- http://www.man-linux-magique.net/man2/dup.html
+	--- http://www.man-linux-magique.net/man3/execl.html
+	--- http://www.man-linux-magique.net/man2/execve.html
+	--- https://fr.wikipedia.org/wiki/Variable_d%27environnement	
+*/
+
+#include "pipex.h"
 
 char	*ft_find_path(char **env)
 {
@@ -34,13 +64,15 @@ char	*ft_get_cmd(char **path, char *cmd)
 	char	*tmp;
 	char	*command;
 
+	if (access(cmd, X_OK) == 0)
+			return (cmd);
 	i = 0;
 	while (path[i])
 	{
-		tmp = ft_strjoin(*path, "/");
+		tmp = ft_strjoin(path[i], "/");
 		command = ft_strjoin(tmp, cmd);
 		free(tmp);
-		if (access(command, 0) == 0)
+		if (access(command, F_OK | X_OK) == 0)
 			return (command);
 		free(command);
 		i++;
@@ -48,7 +80,7 @@ char	*ft_get_cmd(char **path, char *cmd)
 	return (NULL);
 }
 
-void	ft_free_child(char **args)
+void	ft_free_child(char **args, char *cmd)
 {
 	int	i;
 
@@ -59,36 +91,33 @@ void	ft_free_child(char **args)
 		i++;
 	}
 	free(args);
-	ft_putstr_fd("Usage:\n./pipex file1 cmd1 cmd2 file2\n", 2);
-	ft_error(ERR_CMD);
-	exit (1);
+	free(cmd);
+	ft_putstr_fd("Error: command not found\n", 2);
+	exit(EXIT_FAILURE);
 }
 
-void	ft_child(size_t child, t_pipe pipex, char **av, char **env)
+void	ft_child(size_t child, t_pipe *pipex, char **av, char **env)
 {
 	char	*cmd;
 	char	**args;
 
 	if (child == 1)
 	{
-		dup2(pipex.pipe[1], 1);
-		close(pipex.pipe[0]);
-		dup2(pipex.infile, 0);
+		dup2(pipex->pipefd[1], 1);
+		close(pipex->pipefd[0]);
+		dup2(pipex->infile, 0);
 		args = ft_split(av[2], ' ');
 	}
 	if (child == 2)
 	{
-		dup2(pipex.pipe[0], 0);
-		close(pipex.pipe[1]);
-		dup2(pipex.outfile, 1);
+		dup2(pipex->pipefd[0], 0);
+		close(pipex->pipefd[1]);
+		dup2(pipex->outfile, 1);
 		args = ft_split(av[3], ' ');
 	}	
-	cmd = ft_get_cmd(pipex.path, args[0]);
+	cmd = ft_get_cmd(pipex->path, args[0]);
 	if (!cmd)
-	{
-		ft_free_child(args);	
-		free(cmd);
-	}
+		ft_free_child(args, cmd);	
 	execve(cmd, args, env);
 }
 
@@ -107,42 +136,47 @@ void	ft_free_cmd_path(t_pipe *pipex)
 	free(pipex->path);
 }
 
-void	ft_pipex(char **av, char **env)
+void	ft_pipex(t_pipe *pipex, char **av, char **env)
 {
-	t_pipe	pipex;
 	pid_t	pid1;
 	pid_t 	pid2;
 	char	*path;
 
 	path = ft_find_path(env);
-	pipex.path = ft_split(path, ':');
+	pipex->path = ft_split(path, ':');
 	pid1 = fork();
 	if (pid1 == 0)
 		ft_child(1, pipex, av, env);
 	pid2 = fork();
 	if (pid2 == 0)
 		ft_child(2, pipex, av, env);
-	close(pipex.pipe[0]);
+	close(pipex->pipefd[0]);
+	close(pipex->pipefd[1]);
 	waitpid(pid1, NULL, 0);
-	close(pipex.pipe[1]);
 	waitpid(pid2, NULL, 0);
-	ft_free_cmd_path(&pipex);
+	ft_free_cmd_path(pipex);
 }
 
-int	main(int ac, char **av, char **env)
+void	ft_print_error()
+{
+	perror("Error");
+	exit(EXIT_FAILURE);
+}
+
+int	main(int ac, char **av, char **env) 
 {
 	t_pipe	pipex;
 
 	if (ac != 5)
-	{
-		ft_error(ERR_INPUT);
-		ft_putstr_fd("Usage:\n./pipex file1 cmd1 cmd2 file2\n", 2);
-		return (EXIT_FAILURE);
-	}
+		ft_print_error();
+	if (!env || env[0][0] == '\0')
+		ft_print_error();
 	pipex.infile = open(av[1], O_RDONLY);
 	pipex.outfile = open(av[4], O_TRUNC | O_CREAT | O_RDWR, 0644);
 	if (pipex.infile < 0 || pipex.outfile < 0)
-		return (EXIT_FAILURE);
-	ft_pipex(av, env);
+		ft_print_error();
+	if (pipe(pipex.pipefd) == -1) 
+		ft_print_error();
+	ft_pipex(&pipex, av, env);
 	return (0);
 }
--- pipex.h
+++ pipex.h
@@ -3,15 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   pipex.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pperol <pperol@student.42.fr>              +#+  +:+       +#+        */
+/*   By: pperol <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/09/29 16:22:28 by pperol            #+#    #+#             */
-/*   Updated: 2022/10/13 15:48:03 by pperol           ###   ########.fr       */
+/*   Created: 2022/11/14 15:37:55 by pperol            #+#    #+#             */
+/*   Updated: 2022/11/24 17:28:02 by pperol           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PUSH_SWAP_H
-# define PUSH_SWAP_H
+#ifndef PIPEX_H
+# define PIPEX_H
 
 /* write, read, close, access, pipe, fork, dup, execve */
 # include <unistd.h>
@@ -35,11 +35,11 @@
 /* perror*/
 # include <stdio.h>
 
-
 # define ERR_INPUT "Invalid number of arguments.\n"
-# define ERR_ACCESS "No such file or directory\n"
-# define ERR_CMD "Invalid command\n"
-# define ERR "Error\n"
+//# define ERR_ACCESS "No such file or directory\n"
+//# define ERR_CMD "Invalid command\n"
+# define ERR_CMD "Error: command not found"
+# define ERR "Error: "
 
 /*
 ** DATAS ****************
@@ -49,30 +49,30 @@ typedef struct s_pipe
 	int		infile;
 	int		outfile;
 	char	**path;
-	int		pipe[2];
+	int		pipefd[2];
 }	t_pipe;	
 
 /*
 ** PIPEX ******************
 */
-int		ft_error(char *msg);
+void	ft_print_error();
 char	*ft_find_path(char **env);
 char	*ft_get_cmd(char **paths, char *cmd);
-void	ft_free_child(char **args);
-void	ft_child(size_t child, t_pipe pipex, char **argv, char **env);
+void	ft_free_child(char **args, char *cmd);
+void	ft_child(size_t child, t_pipe *pipex, char **argv, char **env);
 void	ft_free_cmd_path(t_pipe *pipex);
-void	ft_pipex(char **av, char **env);
+void	ft_pipex(t_pipe *pipex, char **av, char **env);
 
 /*
 ** LIBFT ******************
 */
-size_t  ft_strlen(const char *s);
+size_t	ft_strlen(const char *s);
 void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
 void	*ft_calloc(size_t nmemb, size_t size);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 char	**ft_split(char const *s, char c);
-int     ft_strncmp(const char *s1, const char *s2, size_t n);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strjoin(char const *s1, char const *s2);
 void	ft_putstr_fd(char *s, int fd);
 
